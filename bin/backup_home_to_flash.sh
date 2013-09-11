77eb0e75dbbc1585e3de96f641357e8ae319f7a1
Bernhard Koenig <b.a.koenig@gmail.com>
2013-09-11 18:53:16 +0200

    bkoenig change

---
+++ bin/backup_home_to_flash.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#mount directory of USB stick
+DIR="/run/media/bkoenig/Rotate"
+
+
+rsync -av /home/bkoenig/Documents $DIR/bkoenig
+rsync -av /home/bkoenig/math $DIR/bkoenig
+rsync -av /home/bkoenig/Public $DIR/bkoenig
+
+rsync -av /home/bkoenig/.thunderbird $DIR/bkoenig
