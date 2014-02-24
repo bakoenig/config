ea676df8f35825a1befa8ec31612a95adbc91821
Bernhard Koenig <b.a.koenig@gmail.com>
2014-02-24 12:03:55 +0100

    re-order layouts

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -62,12 +62,12 @@ modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 local layouts =
 {
+    awful.layout.suit.fair,
+    awful.layout.suit.fair.horizontal,
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
     awful.layout.suit.tile.top,
-    awful.layout.suit.fair,
-    awful.layout.suit.fair.horizontal,
 --    awful.layout.suit.spiral,
 --    awful.layout.suit.spiral.dwindle,
     awful.layout.suit.floating,
@@ -95,7 +95,7 @@ tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
     tags[s] = awful.tag({ " Αα " , " Ββ " , " Γγ " , " Δδ " , " Ωω " }, s,
-    { layouts[5] , layouts[8] , layouts[5] , layouts[5] , layouts[7] })
+    { layouts[1] , layouts[8] , layouts[1] , layouts[1] , layouts[7] })
 end
 -- }}}
 
