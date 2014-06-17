49981e2042beaaa3d0dae6bce74e6607b6fe15eb
Bernhard Koenig <b.a.koenig@gmail.com>
2014-06-17 21:00:29 +0200

    change of theme

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -41,8 +41,8 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
 --beautiful.init("/usr/share/awesome/themes/default/theme.lua")
-beautiful.init("/home/bernhard/.config/awesome/themes/steamburn/theme.lua")
--- other themes: blackburn, dremora, multicolor, steamburn, wombat
+beautiful.init("/home/bernhard/.config/awesome/themes/sand/theme.lua")
+-- other themes: blackburn, sand, orbit, steamburn, wombat
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvt"
