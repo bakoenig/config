5b21eddcf6b4b56c1156d07e4a7a3b70c3b91058
Bernhard Koenig <b.a.koenig@gmail.com>
2014-07-10 02:17:50 +0200

    switch back to PgUp PgDown for Desktop switch

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -341,8 +341,8 @@ root.buttons(awful.util.table.join(
 
 -- {{{ Key bindings
 globalkeys = awful.util.table.join(
-    awful.key({ modkey,           }, "Left", awful.tag.viewprev),
-    awful.key({ modkey,           }, "Right", awful.tag.viewnext),
+    awful.key({ modkey,           }, "Prior", awful.tag.viewprev),
+    awful.key({ modkey,           }, "Next", awful.tag.viewnext),
     awful.key({ modkey,           }, "Escape", awful.tag.history.restore),
 
     awful.key({ modkey,           }, "j",
