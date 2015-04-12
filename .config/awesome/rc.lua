36f4ce5d0e5c5ffdeb9bcac42c4bfbf6b35b6585
Bernhard Koenig <b.a.koenig@gmail.com>
2015-04-13 00:24:22 +0200

    change tags

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -94,7 +94,7 @@ end
 tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ " Α1α " , " Β2β " , " Γ3γ " , " Δ4δ " , " Ω5ω " }, s,
+    tags[s] = awful.tag({ " Αα1 " , " Ββ2 " , " Γγ3 " , " Δδ4 " , " Ωω5 " }, s,
     { layouts[1] , layouts[8] , layouts[1] , layouts[1] , layouts[5] })
 end
 -- }}}
